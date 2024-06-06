f5573b6d564765bc57b4b6f6aa2be6287f67f6e1
Sergei Loshkarev <saloshkarev@miem.hse.ru>
2024-06-06 14:24:12 +0300

    add tasks

--- yap-back/yap/adapters/photo_repository.py
+++ yap-back/yap/adapters/photo_repository.py
@@ -1,11 +1,24 @@
 import io
+import os
 import logging as log
 
 from minio import Minio
 
+ALLOWED_ENV = {'prod', 'test'}
 
-YA_ART_GEN_BUCKET = 'ya-art-generated'
-INPAINTER_GEN_BUCKET = 'inpainter-generated'
+def get_app_env() -> str:
+    app_env = os.getenv('APP_ENV', 'test')
+    if app_env in ALLOWED_ENV:
+        return app_env
+    raise ValueError('Unexpected APP_ENV %r', app_env)
+
+
+def _get_bucket_name(bucket_prefix: str) -> str:
+    return f'{bucket_prefix}-{get_app_env()}'
+
+
+YA_ART_GEN_BUCKET = _get_bucket_name('ya-art-generated')
+INPAINTER_GEN_BUCKET = _get_bucket_name('inpainter-generated')
 
 USED_BUCKETS = [
     YA_ART_GEN_BUCKET,
