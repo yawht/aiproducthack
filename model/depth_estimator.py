6106518c9e6231127d75b017c7bb453a08a2e5f6
Darina Rustamova <darisha6602@gmail.com>
2024-06-11 21:47:21 +0300

    feat: added depth estimator class

--- model/depth_estimator.py
+++ model/depth_estimator.py
@@ -2,44 +2,50 @@ import torch
 import numpy as np
 from PIL import Image
 from transformers import DPTFeatureExtractor, DPTForDepthEstimation
+import logging
 
 depth_estimator = None
 feature_extractor = None
 
 
-def init():
-    global depth_estimator, feature_extractor
+class DepthEstimator:
+    def __init__(
+        self,
+        depth_estimator: str = "Intel/dpt-hybrid-midas",
+        feature_extractor: str = "Intel/dpt-hybrid-midas",
+    ):
+        logging.basicConfig(level=logging.INFO)
+        logging.info("Initializing depth estimator...")
 
-    print("Initializing depth estimator...")
+        self.depth_estimator = DPTForDepthEstimation.from_pretrained(
+            depth_estimator
+        ).to("cuda")
+        self.feature_extractor = DPTFeatureExtractor.from_pretrained(
+            feature_extractor
+        )
 
-    depth_estimator = DPTForDepthEstimation.from_pretrained(
-        "Intel/dpt-hybrid-midas").to("cuda")
-    feature_extractor = DPTFeatureExtractor.from_pretrained(
-        "Intel/dpt-hybrid-midas")
 
+    def get_depth_map(self, image):
+        original_size = image.size
 
-def get_depth_map(image):
-    original_size = image.size
+        image = self.feature_extractor(images=image, return_tensors="pt").pixel_values.to("cuda")
 
-    image = feature_extractor(
-        images=image, return_tensors="pt").pixel_values.to("cuda")
+        with torch.no_grad(), torch.autocast("cuda"):
+            depth_map = self.depth_estimator(image).predicted_depth
 
-    with torch.no_grad(), torch.autocast("cuda"):
-        depth_map = depth_estimator(image).predicted_depth
+        depth_map = torch.nn.functional.interpolate(
+            depth_map.unsqueeze(1),
+            size=original_size[::-1],
+            mode="bicubic",
+            align_corners=False,
+        )
 
-    depth_map = torch.nn.functional.interpolate(
-        depth_map.unsqueeze(1),
-        size=original_size[::-1],
-        mode="bicubic",
-        align_corners=False,
-    )
+        depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
+        depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
+        depth_map = (depth_map - depth_min) / (depth_max - depth_min)
+        image = torch.cat([depth_map] * 3, dim=1)
 
-    depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
-    depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
-    depth_map = (depth_map - depth_min) / (depth_max - depth_min)
-    image = torch.cat([depth_map] * 3, dim=1)
+        image = image.permute(0, 2, 3, 1).cpu().numpy()[0]
+        image = Image.fromarray((image * 255.0).clip(0, 255).astype(np.uint8))
 
-    image = image.permute(0, 2, 3, 1).cpu().numpy()[0]
-    image = Image.fromarray((image * 255.0).clip(0, 255).astype(np.uint8))
-
-    return image
+        return image
