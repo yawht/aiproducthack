1b499ab97d97d601da7745dfbf23a48199bd9ea1
aalimonov_2 <aalimonov_2@miem.hse.ru>
2024-06-11 19:52:00 +0300

    Update settings, add migration

--- yap-back/yap/settings.py
+++ yap-back/yap/settings.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 class AppSettings (BaseSettings):
     pg_url: str = os.getenv('PG_URL', 'postgresql://user:crackme@localhost:5432/yap')
     app_env: str = os.getenv('APP_ENV', 'test')
-    app_host: str = os.getenv('APP_HOST', '0.0.0.0')
+    app_host: str = os.getenv('APP_HOST', '127.0.0.1')
     app_port: str = os.getenv('APP_PORT', '8080')
     minio_endpoint: str = os.getenv('MINIO_ENDPOINT', '127.0.0.1:9000')
     minio_access_key: str = os.getenv('MINIO_ACCESS_KEY', 'minio')
