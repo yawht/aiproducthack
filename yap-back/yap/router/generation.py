1ea3aa2c667370f28e72ac93c50cd09bf26ebb4c
aalimonov_2 <aalimonov_2@miem.hse.ru>
2024-06-12 16:12:06 +0300

    Limit response in list generations

--- yap-back/yap/router/generation.py
+++ yap-back/yap/router/generation.py
@@ -8,14 +8,22 @@ from yap.dependencies import get_db, get_photo_repo
 from yap.adapters.photo_repository import PhotoRepository, YA_ART_SOURCE_BUCKET
 from yap.router.api import CreateGenerationRequest, Generation
 from yap.mapper.generation import map_generation_model
+from yap.settings import settings
 
 
 generation_router = APIRouter()
 
 
 @generation_router.get("/api/generations")
-def list_generations(db: Session = Depends(get_db)) -> list[Generation]:
-    sessionModels = db.query(schema.Generation).order_by(schema.Generation.created_at.desc()).all()
+def list_generations(
+    page: int = 0, size: int = 50, db: Session = Depends(get_db)
+) -> list[Generation]:
+    sessionModels = (
+        db.query(schema.Generation)
+        .order_by(schema.Generation.created_at.desc())
+        .limit(settings.generation_list_limit)
+        .all()
+    )
     return list(map(map_generation_model, sessionModels))
 
 
@@ -36,13 +44,13 @@ def launch_generation(
     photo_repo: PhotoRepository = Depends(get_photo_repo),
     db: Session = Depends(get_db),
 ) -> Generation:
-    
+
     # God i hate RFC2045
     mime_header, encoded_img = request.input_image.split(",")
     if encoded_img is None:
         raise HTTPException(status_code=400, detail="Invalid image format")
-    _, raw_part = mime_header.split('/')
-    img_extension, _ = raw_part.split(';')
+    _, raw_part = mime_header.split("/")
+    img_extension, _ = raw_part.split(";")
     if img_extension not in ["jpeg", "png"]:
         raise HTTPException(status_code=400, detail="Invalid image extension")
 
