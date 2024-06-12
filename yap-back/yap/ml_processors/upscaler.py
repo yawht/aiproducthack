3fa8fcf4b61dea4573952f53c6b60c10e99de8eb
Sergei Loshkarev <saloshkarev@miem.hse.ru>
2024-06-12 17:27:48 +0300

    gg

---
+++ yap-back/yap/ml_processors/upscaler.py
@@ -0,0 +1,42 @@
+# Based on https://github.com/xinntao/Real-ESRGAN/blob/master/inference_realesrgan.py
+import os
+import requests
+
+import cv2
+import numpy as np
+from PIL import Image
+from realesrgan import RealESRGANer
+from basicsr.archs.rrdbnet_arch import RRDBNet
+import logging
+import torch
+
+class Upscaler:
+    def __init__(self, device: str = "cuda"):
+        logging.basicConfig(level=logging.INFO)
+        logging.info("Initializing upscaler...")
+
+        if device == "cuda" and not torch.cuda.is_available():
+            logging.warning("CUDA is not available, using CPU...")
+            device = "cpu"
+        
+        if not os.path.exists("weights"):
+            os.mkdir("weights")
+            url = 'https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth'
+            response = requests.get(url)
+            with open('weights/RealESRGAN_x2plus.pth', 'wb') as f:
+                f.write(response.content)
+        
+        self.model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64,
+                        num_block=23, num_grow_ch=32, scale=2)
+        self.upsampler = RealESRGANer(
+            scale=2, model_path="weights/RealESRGAN_x2plus.pth", model=self.model, device=device)
+
+
+    def upscale(self, image: Image) -> Image:
+        original_numpy = np.array(image)
+        original_opencv = cv2.cvtColor(original_numpy, cv2.COLOR_RGB2BGR)
+
+        output, _ = self.upsampler.enhance(original_opencv, outscale=2)
+        upscaled = Image.fromarray(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
+
+        return upscaled
